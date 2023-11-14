0e68b0a58d6a3c57aaac74024eb3e33db36cef80
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-11-14 19:05:37 +0200

    add database migration 🎭

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // create fakse 20 users
+        \App\Models\User::factory()->count(20)->create();
+    }
+}
