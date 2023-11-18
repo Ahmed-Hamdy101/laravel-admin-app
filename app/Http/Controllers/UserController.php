ee7332b9f36c439857ebf2807c3003194e986f13
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-11-18 21:54:52 +0200

    add all api routes ➰

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,4 +11,31 @@ class UserController extends Controller
     {
         return User::all();
     }
+
+    // get single id
+    public function show($id)
+    {
+      return  User::find($id); 
+    }
+    // store the information
+    public function store(Request $r)
+    {
+        $user = User::create([
+            "f_name"=> $r->input('f_name'),
+            "l_name"=>  $r->input('l_name'),
+            "email"=>  $r->input('email'),
+            "password"=> bcrypt( $r->input('password')),
+        ]);       
+        return response($user , 201);
+    }
+    // update the information
+    public function update()
+    {
+       
+    }
+    // delete the information
+    public function delete()
+    {
+       
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,8 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -30,15 +27,8 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
+ 
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+
 }
