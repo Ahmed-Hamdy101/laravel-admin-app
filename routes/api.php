4c18079392fe09bc22f96bec347c114e2e8b83b6
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-12-12 17:11:10 +0200

    add destroy route :radio_button:

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::get("/users",[UserController::class,'index']);
 Route::get("/users/{id}",[UserController::class,'show']);
 Route::post("/users",[UserController::class,'store']);
 Route::put("/users/{id}",[UserController::class,'update']);
+Route::delete("/users/{id}", [UserController::class, 'destroy']);
+
