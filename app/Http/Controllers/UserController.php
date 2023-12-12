a48d13a93c2c653d68987f6007ef27ccdb6562e9
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-12-12 17:09:44 +0200

    update UserController 🔢

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\UserCreateRequest;
+use App\Http\Requests\UserUpdateRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
 
 class UserController extends Controller
 {
@@ -18,24 +19,30 @@ class UserController extends Controller
       return  User::find($id); 
     }
     // store the information
-    public function store(Request $r)
+    public function store(UserCreateRequest $r)
     {
-        $user = User::create([
-            "f_name"=> $r->input('f_name'),
-            "l_name"=>  $r->input('l_name'),
-            "email"=>  $r->input('email'),
-            "password"=> bcrypt( $r->input('password')),
-        ]);       
-        return response($user , 201);
+        $user = User::create($r->only('f_name','l_name','email') +[ "password"=> bcrypt( $r->input('password'))]);       
+        return response($user , HttpResponse::HTTP_CREATED);
     }
     // update the information
-    public function update()
+    public function update(UserUpdateRequest $r, $id)
     {
-       
+       $userup = User::find($id);
+       $userup->update($r->only('f_name','l_name','email'));
+       return response($userup,HttpResponse::HTTP_ACCEPTED);
     }
     // delete the information
-    public function delete()
+    public function destroy( $id)
     {
-       
-    }
+        try {
+            // Find the user by ID and delete
+            $user = User::findOrFail($id);
+            $user->delete();
+
+            return response()->json(['message' => 'User deleted successfully'], 200);
+        } catch (\Exception $e) {
+            // Handle any exceptions (e.g., user not found)
+            return response()->json(['error' => 'User not found'], 404);
+        }
+}
 }
