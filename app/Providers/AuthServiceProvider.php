18cd33e34df0cb29834c74b35043738380cf6774
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-12-26 14:04:32 +0200

    add api driver 🗂️

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +26,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // Other Passport configurations...
+
+        // Example: set the storage keys for Passport
+        Passport::loadKeysFrom(storage_path('/keys'));
     }
 }
