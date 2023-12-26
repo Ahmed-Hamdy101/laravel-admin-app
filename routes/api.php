0c03e6c229c9f80ddd7dc3e2cfe0c95144b430e2
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-12-26 14:05:46 +0200

    Auth Routes and loign request

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Models\User;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,13 +16,11 @@ use App\Http\Controllers\UserController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum', 'api'])->group(function () {
+    Route::apiResource('/users', UserController::class);
 });
-Route::get("/users",[UserController::class,'index']);
-Route::get("/users/{id}",[UserController::class,'show']);
-Route::post("/users",[UserController::class,'store']);
-Route::put("/users/{id}",[UserController::class,'update']);
-Route::delete("/users/{id}", [UserController::class, 'destroy']);
+
+Route::post('/login', [AuthController::class,'login'])->name('login');
+\\
+Route::post('/users', [UserController::class,'store']);
 
